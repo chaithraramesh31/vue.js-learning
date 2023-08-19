c5f4a2826e4c527d626e652c8366d583584364f8
Chaithra R <chaithraramesh31@gmail.com>
2023-08-19 23:57:39 +0530

    Working with forms

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -6,19 +6,24 @@
             :nav-link-click="(index) => activePage = index"
         >
         </navbar>
-        <page-view 
+        <!-- <page-view 
             :page="pages[activePage]"
-        ></page-view>
+        ></page-view> -->
+        <create-page
+            :page-created="pageCreated"
+        ></create-page>
     </div>
 </template>
 <!-- v-if="pages.length > 0" -->
 <script>
 import Navbar from './components/Navbar.vue';
 import PageView from './components/PageView.vue';
+import CreatePage from './components/CreatePage.vue';
 export default {
     components: {
         Navbar,
-        PageView
+        PageView,
+        CreatePage
     },
     created() {
         this.getPages()
@@ -34,7 +39,10 @@ export default {
             let res = await fetch('pages.json');
             let data = await res.json();
             this.pages = data;
-        }
+        },
+        pageCreated(pageObj) {
+            console.log(pageObj);
+        },
     }
 }
 </script>
---
+++ vue-start-spa/src/components/CreatePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container mb-3">
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="form-label">Page Title</label>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    v-model="pageTitle"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Content</label>
+                <textarea type="text" rows="5" class="form-control" v-model="content"></textarea>
+            </div>
+            <div class="mb-3">
+                <button 
+                    class="btn btn-primary"
+                    @click.prevent="pageCreated({pageTitle, content})"
+                >Create Page</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['pageCreated'],
+    data() {
+        return{
+            pageTitle: '',
+            content: ''
+        }
+    }
+}
+</script>
