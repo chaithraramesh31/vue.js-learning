acbcd53945af2d8b06389d5f3a450b1476b7bba4
Chaithra R <chaithraramesh31@gmail.com>
2023-08-20 16:33:08 +0530

    Introducing Composition API

--- vue-start-spa/src/views/PagesList.vue
+++ vue-start-spa/src/views/PagesList.vue
@@ -1,3 +1,28 @@
 <template>
-    <p>This is the pages list</p>
+    <div>
+        <p>{{data.counter}}</p>
+        <button @click.prevent="click" class="btn btn-primary">Increment</button>
+    </div>
 </template>
+
+<script setup>
+import {ref, reactive} from 'vue';
+// const counter = ref(0); // this is similar to {value: 0}
+const data = reactive({counter: 0});
+function click() {
+    // counter.value++
+    data.counter++;
+}
+// export default {
+//     data() {
+//         return {
+//             counter: 0
+//         }
+//     },
+//     methods: {
+//         click(){
+//             this.counter++;
+//         }
+//     },
+// }
+</script>
