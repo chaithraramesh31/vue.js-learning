71175175a21ee4a0243de3f5f53e20c284dca01d
Chaithra R <chaithraramesh31@gmail.com>
2023-08-20 11:50:10 +0530

    Using Watchers

--- vue-start-spa/src/components/CreatePage.vue
+++ vue-start-spa/src/components/CreatePage.vue
@@ -1,5 +1,5 @@
 <template>
-    <form action="" class="container mb-3">
+    <form action="" class="container mb-3 mt-3">
         <div class="row">
             <div class="col-md-8">
                 <div class="mb-3">
@@ -75,6 +75,13 @@ export default {
             this.linkText = '',
             this.linkUrl = '',
             this.published = true
+        }
+    },
+    watch: {
+        pageTitle(newTitle, oldTitle) {
+            if(this.linkText == oldTitle){
+                this.linkText = newTitle
+            }
         }
     }
 }
